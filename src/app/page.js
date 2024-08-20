ffa90c7cc46aa2bc21546f79bf6d599748782a10
Christopher Negron <sonycar2000@outlook.com>
2024-08-20 13:48:36 -0400

    OrangeButton set up

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import OrangeButton from "@/components/buttons/OrangeButton/OrangeButton";
+import garageImage from "@/imgs/NavBar/garage.svg";
+
 export default function Home() {
   return (
     <main className={styles.main}>
-      <OrangeButton />
+      <OrangeButton
+        svgIcon={garageImage}
+        altText="home"
+        goToPage="@/app/page"
+      />
     </main>
   );
 }
